0c91115d7827102e8f129c7abd2231d02eea2216
Joaolpridolficarvalho <jlprcarvalho@gmail.com>
2025-08-25 10:23:10 -0300

    Script funcional

--- script/processa_issue.py
+++ script/processa_issue.py
@@ -1,20 +1,20 @@
 import re
 import json
+import sys
 
-NOME_ARQUIVO = "./crud_liquido_issue.json"
-def parse_issue() -> dict:
+def parse_issue(nome_arquivo : str) -> dict:
     """
     Recebe um JSON de issue do GitHub (dict) e retorna outro JSON (dict)
     com os campos do body estruturados + label principal.
     """
-    with open(NOME_ARQUIVO, "r", encoding="utf-8") as f:
+    with open(nome_arquivo, "r", encoding="utf-8") as f:
         issue = json.load(f)
     # pega só o nome do primeiro label (se existir)
     labels = [label["name"] for label in issue.get("labels", [])]
     label = labels[0] if labels else ""
 
     resultado = {
-        "number": str(issue.get("number", "")),
+        
         "title": issue.get("title", ""),
         "label": label,
         "labels": labels,
@@ -33,40 +33,13 @@ def parse_issue() -> dict:
     for campo, valor in padrao_negrito.findall(body):
         chave = campo.strip().lower().replace(" ", "_")
         resultado[chave] = valor.strip()
-
-    # label
-
-    return resultado
+    
+    print(resultado)
+    with open(nome_arquivo, "w", encoding="utf-8") as f:
+        json.dump(resultado, f, indent=2, ensure_ascii=False)
 
 
 # Exemplo de uso
 if __name__ == "__main__":
-    issue = {
-        "number": "7",
-        "title": "adicionar evento",
-        "body": """### Nome do Evento
-
-teste 333333333
-
-### Formato do Evento
-
-Remoto
-
-### Ano do Evento
-
-2025
-### Descrição
-teste 3333333
-### data do evento
-2025-12-31
-### Link do Evento
-https://www.teste.com
-""",
-        "labels": [
-            {"name": "adicionar"}
-        ],
-        "user": "Joaolpridolficarvalho"
-    }
-
-    parsed = parse_issue()
-    print(json.dumps(parsed, indent=2, ensure_ascii=False))
+    arquivo = sys.argv[1] 
+    parse_issue(arquivo)
